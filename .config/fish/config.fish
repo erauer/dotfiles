2e006013d7066ae8028a4628f8d63e5d8bce3a03
Eric Rauer <eric.rauer+gh@gmail.com>
2017-10-07 09:30:35 -0700

    Support vi mode for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,14 @@ source $OMF_PATH/init.fish
 source ~/.asdf/asdf.fish
 
 set fish_greeting
+set fish_vi_key_bindings fzf_key_bindings
 
+set -gx LSCOLORS gxfxcxdxbxegedabagacad
+
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /Users/erauer/.asdf/installs/nodejs/6.10.0/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/erauer/.asdf/installs/nodejs/6.10.0/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /Users/erauer/.asdf/installs/nodejs/6.10.0/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /Users/erauer/.asdf/installs/nodejs/6.10.0/.npm/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
