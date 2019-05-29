9ad45276627e2473159f187468c07972f3dd82e2
Eric Rauer <eric.rauer+gh@gmail.com>
2019-05-29 11:06:18 -0700

    more updates

--- .config/i3/scripts/laptop-display.sh
+++ .config/i3/scripts/laptop-display.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-3 --off --output eDP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal
+xrandr --output DP-1 --off --output eDP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal
