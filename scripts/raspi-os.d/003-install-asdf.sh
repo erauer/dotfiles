eb0a657bb63db7ee1491a12f50c181bc8ea323bf
Eric Rauer <eric.rauer+gh@gmail.com>
2020-08-04 23:52:02 +0100

    Add golang plugin configuration

--- scripts/raspi-os.d/003-install-asdf.sh
+++ scripts/raspi-os.d/003-install-asdf.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0-rc1
+
+source . ~/.asdf/asdf.sh
+
+# sudo apt-get install libssl-dev libncurses5-dev build-essential -y
+
+# asdf plugin-add elixir
+
+# asdf plugin-add erlang
+
+asdf plugin-add golang
+
+asdf install
+
